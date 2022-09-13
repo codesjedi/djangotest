71e8f8daf5e0d99107eef201e617a3d649cbccbb
Enrique Gimenez <me@enrique.digital>
2022-09-13 13:21:45 -0400

    hello world

--- djangotest/urls.py
+++ djangotest/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import hello, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', hello),
+    path('about/', about)
 ]
